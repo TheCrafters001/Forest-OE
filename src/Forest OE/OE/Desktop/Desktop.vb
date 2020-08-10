03b1c4226fd0267710097227c919e681d3a95a30
TheCrafters001 <46639269+TheCrafters001@users.noreply.github.com>
2020-08-10 12:45:22 -0400

    Fixing issues

--- src/Forest OE/OE/Desktop/Desktop.vb
+++ src/Forest OE/OE/Desktop/Desktop.vb
@@ -9,73 +9,7 @@ Public Class Desktop
         Time_lbl.Text = TimeOfDay.ToString("h:mm:ss tt")
     End Sub
 
-    Private Sub ShutdownToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ShutdownToolStripMenuItem.Click
+    Private Sub shutdownMenuItem_Desktop_Click(sender As Object, e As EventArgs) Handles shutdownMenuItem_Desktop.Click
         ShutdownDialog.Show()
-
-    End Sub
-
-    Private Sub SettingsToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SettingsToolStripMenuItem.Click
-        Settings.Show()
-    End Sub
-
-    Private Sub AboutToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AboutToolStripMenuItem.Click
-        About.Show()
-    End Sub
-
-    Private Sub FileExplorerToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles FileExplorerToolStripMenuItem.Click
-        Explorer.Show()
-    End Sub
-
-    Private Sub RunToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles RunToolStripMenuItem.Click
-        Run.Show()
-    End Sub
-
-    Private Sub ChromeToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ChromeToolStripMenuItem.Click
-        Try
-            If Not My.Computer.FileSystem.DirectoryExists("C:\Program Files (x86)\Google\Chrome\Application") Then ' 64-bit
-                Process.Start("C:\Program Files\Google\Chrome\Application\chrome.exe")
-            ElseIf Not My.Computer.FileSystem.DirectoryExists("C:\Program Files\Google\Chrome\Application") Then ' 32-bit
-                Process.Start("C:\Program Files (x86)\Google\Chrome\Application\chrome.exe")
-            End If
-        Catch ex As Exception
-            CrashScreen.Show()
-            CrashScreen.Label2.Text = "What Caused Crash: Application Launch Event  Failed"
-            CrashScreen.Label5.Text = ex.Message
-        End Try
-    End Sub
-
-    Private Sub InternetExplorerToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles InternetExplorerToolStripMenuItem.Click
-        Try
-            If Not My.Computer.FileSystem.DirectoryExists("C:\Program Files (x86)\Internet Explorer\") Then
-                Process.Start("C:\Program Files\Internet Explorer\iexplore.exe")
-            ElseIf Not My.Computer.FileSystem.DirectoryExists("C:\Program Files\Internet Explorer\") Then
-                Process.Start("C:\Program Files (x86)\Internet Explorer\iexplore.exe")
-            End If
-        Catch ex As Exception
-            CrashScreen.Show()
-            CrashScreen.Label2.Text = "What Caused Crash: Application Launch Event  Failed"
-            CrashScreen.Label5.Text = ex.Message
-        End Try
-    End Sub
-    Private Sub CommandPromptToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CommandPromptToolStripMenuItem.Click
-        Dim ProgramStarter As New ProcessStartInfo
-        ProgramStarter.FileName = "Forest-OS-Console.exe"
-        ProgramStarter.Arguments = ""
-        ProgramStarter.UseShellExecute = True
-        ProgramStarter.WindowStyle = ProcessWindowStyle.Normal
-        Dim proc As Process = Process.Start(ProgramStarter)
-    End Sub
-
-    Private Sub CMDWindowsToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CMDWindowsToolStripMenuItem.Click
-        Dim ProgramStarter As New ProcessStartInfo
-        ProgramStarter.FileName = "cmd.exe"
-        ProgramStarter.Arguments = ""
-        ProgramStarter.UseShellExecute = True
-        ProgramStarter.WindowStyle = ProcessWindowStyle.Normal
-        Dim proc As Process = Process.Start(ProgramStarter)
-    End Sub
-
-    Private Sub ListView1_SelectedIndexChanged(sender As Object, e As EventArgs)
-
     End Sub
 End Class
